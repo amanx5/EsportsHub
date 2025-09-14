82f5773cd5c42d71bd66cb1038ed2835682369d6
amanx5 <110730645+amanx5@users.noreply.github.com>
2025-09-14 23:57:13 +0530

    Problem with NavData | Correction

--- ui/src/pages/tournament/Tournament.jsx
+++ ui/src/pages/tournament/Tournament.jsx
@@ -9,12 +9,12 @@ export default function () {
 		<div>tournaments</div>
 		<div>
 			{
-				// *** ISSUE IS HERE ***
-				// problem with NavData - value doesn't update here on expanding/closing menubutton 
+				// value won't update here on expanding/closing menubutton 
 				// as tournaments won’t re-render when enableMobileMenu changes
 				// since enableMobileMenu is state within Navbar only
 				'value = ' + NavData.enableMobileMenu 
-				// if useContext was used, React would subscribe Tournaments to that state. so tournaments would also get re rendered
+				// even if useContext was used, it would also don't react as this component is not subscribed to the state
+				// if we move enableMobileMenu state one level up, both approach will give start working, so this is not main issue
 			}
 		</div>
 		</>
